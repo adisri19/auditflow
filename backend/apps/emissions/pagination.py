61d02aa8c731d0d9bbdb33c7c12ea85e4374c80d
Aditya Srivastava <adityasrivastava9454@gmail.com>
2026-09-06 09:47:16 +0530

    feat: replace manual Paginator with DRF PageNumberPagination class

---
+++ backend/apps/emissions/pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class RecordPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'results': data,
+            'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'has_next': self.page.has_next(),
+            'has_previous': self.page.has_previous()
+        })
